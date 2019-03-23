029a57c31d245fdba421eec115ca11571e179228
tvtremor <tvtremor@gmail.com>
2019-03-23 13:32:22 +0100

    Massive website overhaul
    - New CSS tags updated and integrated into the website
    - Synchronized visuals on all HRT pages and the SRTV Knin page
    - Some text updated, added links to external sites in the about.html page
    - New clock added, fixed problems with thumbnails

--- bin/main.js
+++ bin/main.js
@@ -1,23 +1,26 @@
-function JumbotronCookieSet(){
+/* TVtremor JavaScript/jQuery
+Autor/Author: TVtremor
+Godina/Year: 2019
+
+To do:
+Dark mode/Tamni mod
+
+*/
+
+function jumbotronCookieSet(){
 localStorage.setItem("showjumbotron", "false");
-SetJumbotron();
+setNotifierJumbotron();
 }
 
-function SetJumbotron()
+function setNotifierJumbotron()
 	{
-		var JumbotronSetting = localStorage.getItem("showjumbotron");
-		var JumbotronObject = document.getElementsByClassName("RedNotify");
+		var notifierJumbotronSetting = localStorage.getItem("showjumbotron");
+		var notifierJumbotronObject = document.getElementsByClassName("notifier-jumbotron");
 
-		if(JumbotronSetting=="false")
-			{
-				JumbotronObject[0].style.display = "none";
-			}
-	
+		if(notifierJumbotronSetting=="false")
+			{notifierJumbotronObject[0].style.display = "none";}
 		else
-			{
-				JumbotronObject[0].style.display = "block";
-			}
-
+			{notifierJumbotronObject[0].style.display = "block";}
 	}
 
 $(document).on("click",".extendable-pic", function () {
@@ -28,10 +31,10 @@ $(document).on("click",".extendable-pic", function () {
 });
 
 function Anniversaries(){
-var Today = new Date();
-var Month = Today.getMonth();
-var DayOfMonth = Today.getDate();
-var Year = Today.getFullYear();
+var today = new Date();
+var month = Today.getMonth();
+var dayOfMonth = Today.getDate();
+var year = Today.getFullYear();
 var RZAnniversary = Year-1926;
 var TVAnniversary = Year-1956;
 

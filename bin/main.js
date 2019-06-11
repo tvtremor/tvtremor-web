4da4c3ab58f80de6a4d6396198fd17fc732dba49
tvtremor <tvtremor@gmail.com>
2019-06-11 21:57:06 +0200

    Beginning of the BS4 merge
    Merging project to Bootstrap 4.
    
    - rewritten dropdown menu with unified dark/light mode option
    - removed manual centering for content (primarily jumbotrons)
    - slimming of the CSS file and some classes removed
    
    Content changes:
    - TV stations split to Croatia and Bosnia and Herzegovina

--- bin/main.js
+++ bin/main.js
@@ -6,44 +6,41 @@ To do:
 Dark mode/Tamni mod
 
 */
-var siteVersion = "";
 
 function doPageWork(){
-	getSiteVersion();
 	checkDarkMode();
 }
 
-function getSiteVersion(){	
-fetch('/version.txt')
-  .then(function(response) {
-    return response.text();
-  })
-  .then(function(text) {
-    document.getElementById("footer-version").innerHTML = text;
-  });
-}
-
 function checkDarkMode(){
-	/*var darkModeSelection = localStorage.getItem("dark_mode");
+	var darkModeSelection = localStorage.getItem("dark_mode");
 	
-	if(modeSelection=="true"){
-		toggleDarkMode();
+	if(darkModeSelection=="true"){
+		darkModeApply();
 	}
 	else{
 		null;
-	}*/
+	}
 	null;
 }
 
 function toggleDarkMode(){
-	/*var modeSelection = localStorage.getItem("dark_mode");
-	if(modeSelection=="false" || modeSelection=="" || modeSelection==undefined)
+	var modeSelection = localStorage.getItem("dark_mode");
+	if(modeSelection=="false" || modeSelection=="" || modeSelection==undefined || modeSelection == null)
 		localStorage.setItem("dark_mode", "true");
 	else if (modeSelection=="true")
 		localStorage.setItem("dark_mode", "false");
+	
 	location.reload();
-	*/
 	null;
+}
+
+function darkModeApply(){
+	var darkModeStyleDef = "body{background-color:rgb(15,15,15);}.bg-light{background-color:#070605 !important;}.jumbotron{background-color:#050505;}h1,h2,h3,h4,h5,h6,p{filter:invert(100%);}p > a{filter:invert(100%) !important;}";
+	
+	var darkModeStyle = document.createElement("style");
+	darkModeStyle.type = "text/css";
+	darkModeStyle.innerText = darkModeStyleDef;
+	document.head.appendChild(darkModeStyle);
 }
 
 $(document).on("click",".extendable-pic", function () {
@@ -61,7 +58,7 @@ var year = Today.getFullYear();
 var RZAnniversary = Year-1926;
 var TVAnniversary = Year-1956;
 
-if(Month == 4 && DayOfMonth == 15){
+if(month == 4 && dayOfMonth == 15){
 //
 }
 }
